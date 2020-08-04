fef62adf8c36473dd649337ef6edd4cc832d3671
treverandersen <trever.r.andersen@gmail.com>
2020-08-04 17:42:28 -0500

    setup routes to show all posts for categories and the series, got rid of tags

--- videos-api/config/routes.rb
+++ videos-api/config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      resources :categories do
+        resources :posts, only: [:index]
+      end
+
+      resources :series do 
+        resources :posts, only: [:index]
+      end
+
       resources :posts
       resources :series
-      resources :tags
       resources :categories
     end
   end
